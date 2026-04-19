2d8251be6e3cdf2f57ca311a7d6716a0b492187c
nonehelix <114215575+nonehelix@users.noreply.github.com>
2026-04-19 16:09:19 +0200

    Add playerGui initialization in GameFeatures.lua

--- GameFeatures.lua
+++ GameFeatures.lua
@@ -3,7 +3,8 @@ return function(Shared)
 	local Workspace = Shared.Workspace
 	local RunService = Shared.RunService
 	local RegisterFeature = Shared.RegisterFeature
-	
+	local playerGui = player:WaitForChild("PlayerGui")
+
     local CardRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("Card")
 	local CardConfig = require(
 		ReplicatedStorage:WaitForChild("Modules")

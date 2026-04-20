5daddec632197854757b9dad18b789f6c8e0412d
nonehelix <114215575+nonehelix@users.noreply.github.com>
2026-04-20 12:56:56 +0200

    Update AnimeCardCollection.lua

--- AnimeCardCollection/AnimeCardCollection.lua
+++ AnimeCardCollection/AnimeCardCollection.lua
@@ -177,7 +177,7 @@ return {
 			local Feature = RegisterFeature({
 				Key = "AutoBuy",
 				Tab = "Card",
-				Section = "Buying"
+				Section = "Buying",
 				Order = 10,
 
 				Defaults = {
@@ -356,7 +356,7 @@ return {
 			local Feature = RegisterFeature({
 				Key = "AutoGrade",
 				Tab = "Card",
-				Section = "Grading"
+				Section = "Grading",
 				Order = 15,
 
 				Defaults = {
